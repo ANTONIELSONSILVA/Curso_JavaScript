320713a9579d5d3bb57bac8b545914723b9a0732
ANTONIELSON SILVA <48030260+ANTONIELSONSILVA@users.noreply.github.com>
2021-12-30 02:10:32 -0300

    a

--- Aula5_variavel/Exerci.js
+++
@@ -1,8 +0,0 @@
-let anus = 50;
-let nome = "Chupa Rola";
-let peso = 800;
-let altura = 100;
-
-
-console.log(`${nome} tem`, anus, 'anos, pesa', peso, 'kg');
-console.log('tem', altura,"de altura e seu IMC é de", peso/altura);
