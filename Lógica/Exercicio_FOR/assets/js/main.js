aaf4deec0b229873718b9ab595c851f77be48acf
ANTONIELSONSILVA <antonielsom19@gmail.com>
2022-01-06 14:42:40 -0300

    up

--- Lógica/Exercicio_FOR/assets/js/main.js
+++ Lógica/Exercicio_FOR/assets/js/main.js
@@ -5,23 +5,31 @@ const elementos = [
     {tag: 'section', texto: 'Frase 4'}
 ];
 
-console.log(elementos.length)
 
 function criaElemento(elemento) {
     const E = document.createElement(elemento);
     return E;
 }
 
+display(elementos);
+
 
 
 function display(elementos) {
 
-    const display = document.querySelector('#display');
-    
-    for(i = 0; i < elementos.length; i++){
+    const display = document.querySelector('#container');
 
-        criaElemento()
+    const elem = elementos[0].tag;
+    const msg = elementos[0].texto;
+    const e;
+    console.log(elem);
+    console.log(msg);
 
-    }
+
+    e = criaElemento(elem);
+
+    e.innerHTML = msg;
+    display.appendChild(e);
 
 }
+
