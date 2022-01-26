e88516207605ff4282460654c284ed013af98f13
ANTONIELSONSILVA <antonielsom19@gmail.com>
2022-01-25 23:10:24 -0300

    up

---
+++ Lógica/Pagina_Divisão/assets/js/main.js
@@ -0,0 +1,14 @@
+// Escreva uma função que receba um número e 
+// retorne o seguinte:
+// Número é divisível por 3 = Fizz
+// Número é divisível por 5 = Buzz
+// Número NÃO é divisível por 3 e 5 = retorna o próprio número
+// checar se o número é realmente um número
+// Use a função com números de 0 a 100
+
+const container = document.querySelector('#container');
+
+container.addEventListener()
+
+
+
